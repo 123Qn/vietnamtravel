99382d5afd8939258aa5da000769e939eca69961
123Qn <quanhuynhvt2004@gmail.com>
2025-10-18 16:57:03 +1100

    add

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
