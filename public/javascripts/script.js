4cf135a4a6362848607ef2a1ceaa04149043b5f9
123Qn <quanhuynhvt2004@gmail.com>
2025-10-19 16:08:04 +1100

    finish homepage

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
   const btn = document.getElementById('destinationButton');
-  btn.addEventListener('click', () => {
-  window.location.href ='https://facebook.com';
-  });
+  if (btn) {
+    btn.addEventListener('click', () => {
+      window.location.href = '#';
+    });
+  }
+// image slider 
+const images = [
+    '/images/s1.jpg',
+    '/images/s2.jpg',
+    '/images/s3.jpg',
+    '/images/s4.jpg'
+  ];
+
+  const slide = document.getElementById('slide');
+  let index = 0;
+
+  function nextImage() {
+    index = (index + 1) % images.length; // loop back to 0 after last
+    slide.style.opacity = 0;             // fade out
+
+    setTimeout(() => {
+      slide.src = images[index];         // change image
+      slide.style.opacity = 1;           // fade in
+    }, 500); // half second fade
+  }
+
+  setInterval(nextImage, 2000); // change every 2 seconds
+});
--- routes/home.js
+++ routes/home.js
@@ -6,8 +6,10 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM destinations LIMIT 6");
+    const eventResult = await pool.query("SELECT * FROM events LIMIT 6");
+    const events = eventResult.rows;
     const destinations = result.rows;
-    res.render("home", { title: "Vietnam Travel", destinations });
+    res.render("home", { title: "Vietnam Travel", destinations, events });
   } catch (err) {
     console.error(err);
     res.status(500).send("Database error");
